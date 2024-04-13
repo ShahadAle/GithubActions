5991e9757e1551a78356af77ad32610bcee1ccee
salelew2 <shahadalelew4@gmail.com>
2024-04-12 19:47:52 -0500

    fixed error

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -33,7 +33,7 @@ namespace GithubActionsLab
         [Test]
         public void Subtract_Valid()
         {
-            Assert.AreEqual(1, Program.Subtract("4", "2"));
+            Assert.AreEqual(1, Program.Subtract("3", "2"));
             Assert.AreEqual(-1, Program.Subtract("1", "2"));
             Assert.AreEqual(0, Program.Subtract("5", "5"));
         }
